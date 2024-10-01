6e04075a7cfe41c622bf01b79596ddf5694983aa
Yanyifan Liao <ben.jaminlyyf21@gmail.com>
2024-10-02 00:20:34 +0200

    Initialized project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Chip8Emulator.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+    // ReSharper disable once MemberCanBeMadeStatic.Global
+    public string Greeting => "Welcome to Avalonia!";
+#pragma warning restore CA1822 // Mark members as static
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Chip8Emulator.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
